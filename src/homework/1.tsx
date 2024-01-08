61dde02da38d01b6ee016b0411e87ad16da8cca8
Serhii-Kuzmin <seryy8248@gmail.com>
2024-01-08 17:19:22 +0100

    update 1.tsx

--- src/homework/1.tsx
+++ src/homework/1.tsx
@@ -1,50 +1,6 @@
-// import React, { useEffect, useRef } from 'react';
-
-// // Опишіть Props
-
-// type Props = {
-//   children: React.ReactNode;
-//   onContentEndVisible: () => void;
-// }
-
-// export function Observer({ children, onContentEndVisible }: Props) {
-//   // Вкажіть правильний тип для useRef зверніть увагу, в який DOM елемент ми його передаємо
-//   const endContentRef = useRef<HTMLDivElement>(null);
-
-//   useEffect(() => {
-//     // Вкажіть правильний тип для options, підказка, клас також можна вказувати як тип
-//     const options: IntersectionObserverInit = {
-//       rootMargin: '0px',
-//       threshold: 1.0,
-//       root: null,
-//     };
-
-//     const observer = new IntersectionObserver((entries) => {
-//       entries.forEach((entry) => {
-//         if (entry.intersectionRatio > 0) {
-//           onContentEndVisible();
-//           observer.disconnect();
-//         }
-//       });
-//     }, options);
-
-//     if (endContentRef.current) {
-//       observer.observe(endContentRef.current);
-//     }
-
-//     return () => {
-//       observer.disconnect();
-//     };
-//   }, [onContentEndVisible]);
-
-//   return (
-//     <div>
-//       {children}
-//       <div ref={endContentRef} />
-//     </div>
-//   );
-// }
 import React, { useEffect, useRef } from "react";
+
+// Опишіть Props
 
 type Props = {
   children: React.ReactNode;
@@ -52,9 +8,11 @@ type Props = {
 };
 
 export function Observer({ children, onContentEndVisible }: Props) {
+  // Вкажіть правильний тип для useRef зверніть увагу, в який DOM елемент ми його передаємо
   const endContentRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
+    // Вкажіть правильний тип для options, підказка, клас також можна вказувати як тип
     const options: IntersectionObserverInit = {
       rootMargin: "0px",
       threshold: 1.0,
@@ -74,7 +32,7 @@ export function Observer({ children, onContentEndVisible }: Props) {
       observer.observe(endContentRef.current);
     }
 
-    return (): void => {
+    return () => {
       observer.disconnect();
     };
   }, [onContentEndVisible]);
