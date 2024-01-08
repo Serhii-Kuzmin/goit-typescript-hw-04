c40e09ba59b75ce5ab39225b59a1ef2c58dd096b
Serhii-Kuzmin <seryy8248@gmail.com>
2024-01-08 17:01:12 +0100

    update 4.tsx

--- src/homework/4.tsx
+++ src/homework/4.tsx
@@ -6,19 +6,28 @@ type Menu = { id: MenuIds; title: string };
 
 // Додати тип Menu Selected
 
+type MenuSelected = { selectedMenu: { id?: MenuIds } };
+
 const MenuSelectedContext = createContext<MenuSelected>({
   selectedMenu: {},
 });
 
 // Додайте тип MenuAction
 
+type MenuAction = {
+  onSelectedMenu: React.Dispatch<
+    React.SetStateAction<MenuSelected["selectedMenu"]>
+  >;
+};
+
 const MenuActionContext = createContext<MenuAction>({
   onSelectedMenu: noop,
 });
 
 type PropsProvider = {
-  children; // Додати тип для children
+  children: React.ReactNode; // Додати тип для children
 };
+type SelectedMenu = {};
 
 function MenuProvider({ children }: PropsProvider) {
   // Додати тип для SelectedMenu він повинен містити { id }
@@ -48,7 +57,7 @@ function MenuProvider({ children }: PropsProvider) {
 }
 
 type PropsMenu = {
-  menus; // Додайте вірний тип для меню
+  menus: Menu[]; // Додайте вірний тип для меню
 };
 
 function MenuComponent({ menus }: PropsMenu) {
